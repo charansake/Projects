780b93115b7824143d6f5f5de0da61d9728fc664
Charan <ssscharan5555@gmail.com>
2024-12-18 16:25:10 +0530

    adding the codes

---
+++ app.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Get the button element by its ID
+const button = document.getElementById('clickButton');
+
+// Add an event listener to the button for the 'click' event
+button.addEventListener('click', function() {
+    // Display an alert when the button is clicked
+    alert('Button was clicked!');
+});
